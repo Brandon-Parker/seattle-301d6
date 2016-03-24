94f3d7638b892e4aa1816728dc5dfeeb26896d5f
BRANDON PARKER <Brandon@dkparker.com>
2016-03-24 13:37:22 -0700

    next two comments done on articleView (handleFilters and allCategories)

--- class-14-managing-state/pair-assignment/starter-code/scripts/articleView.js
+++ class-14-managing-state/pair-assignment/starter-code/scripts/articleView.js
@@ -13,19 +13,30 @@
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
+  /* Creating a new method called populateFilters on the articleView object and passing an anonymous function.
+    We are defining a new variable of template and then calling the Handlebars compile method on the id of option-template
+    and grabbing the text. The next thing it does is define options as a new variable and setting it equal to the allAuthors method
+    on the Article constructor and then running a .map method to create a new array function and passing in a parameter of author.
+    It will then return template, which was just defined above it and the post the values of the authors. We are then checking to see
+    if the id author-filter option has any duplicates and then appending it to the option list.
+  */
   articleView.populateFilters = function() {
-    var options,
-      template = Handlebars.compile($('#option-template').text());
+    var template = Handlebars.compile($('#option-template').text());
 
     // Example of using model method with FP, synchronous approach:
     // NB: This method is dependant on info being in the DOM. Only authors of shown articles are loaded.
-    options = Article.allAuthors().map(function(author) { return template({val: author}); });
+    var options = Article.allAuthors().map(function(author) { return template({val: author}); });
     if ($('#author-filter option').length < 2) { // Prevent duplication
       $('#author-filter').append(options);
     };
 
     // Example of using model method with async, SQL-based approach:
     // This approach is DOM-independent, since it reads from the DB directly.
+    /*we are calling the allCategories method on the Article constructor and passing in a function with a
+    parameter of rows. It will then run an if statement to query the DOM to check if the option list under the
+    id of category-filter has a length of less than two. If it does then we call an append method which
+    will then pass a map method to iterate through each row and return the template of that row's specific category.
+    */
     Article.allCategories(function(rows) {
       if ($('#category-filter option').length < 2) {
         $('#category-filter').append(
@@ -38,6 +49,10 @@
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
+  /*Creating the handleFilters method on the articleView object and creating an anonymous function
+  which queries the dom for the Id of filters and on a single change event, it changes the select the element and creates a new function
+  that changes the '-filter' into a blank string. and then replaces any/all whitespace with a +
+  */
   articleView.handleFilters = function() {
     $('#filters').one('change', 'select', function() {
       resource = this.id.replace('-filter', '');
