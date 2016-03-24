5cfe79ffb8c5de54e94d43598213d71461d44977
bonecrushereb <bonecrushereb@gmail.com>
2016-03-24 13:05:30 -0700

    we commented on the loadAll and loadByCategory method

--- class-14-managing-state/pair-assignment/starter-code/scripts/articleController.js
+++ class-14-managing-state/pair-assignment/starter-code/scripts/articleController.js
@@ -49,23 +49,41 @@
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
+  /*Creating a new method called loadByCategory on the articlesController module and passing in two parameters of the context object and next callback.
+  We are creating a function and passing in parameter of articles in category. We are then setting the property of articles eqaul to articlesInCategory. 
+  */
   articlesController.loadByCategory = function(ctx, next) {
     var categoryData = function(articlesInCategory) {
       ctx.articles = articlesInCategory;
       next();
     };
-
+    /* Next we are calling the findWhere method and passing in the three required arguments of 'category' as the "fields" element,
+	the value as the ctx.params element and the callback as categoryData. Then it runs the webDB execute method which uses SQL to select everything from the aricles table where the "field" ('category')
+	element is equal to categoryName parameter to context object. 
+	*/
     Article.findWhere('category', ctx.params.categoryName, categoryData);
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
-  //Apply a property of articles to the ctx object
+  /*We are creating a new method called loadAll on the articlesController module and creating a new function that passes in two parameters of ctx and next.
+  We are then creating a new variable called ctx.articles which is equal to the artile.all array. Then we are calling the "next" callback.
+  */
   articlesController.loadAll = function(ctx, next) {
     var articleData = function(allArticles) {
       ctx.articles = Article.all;
       next();
     };
 
+	/* We are creating an if statement and checking to see if there is anything in Article.all. If there is anything we set article.all to the context.articles object.
+	IF not we run the fetchAll method on the Article constructor and passing in a parameter of articleData.
+	The fetchAll method on the Article constructor has a function with a paramater of callback and is a webDB method(excute) to select everything from the articles table in order by the pusblishedON description and
+	then creating another callback function with a parameter of rows. We are then using an if statement to check if the rows has anything in them and then calling the loadAll method on the article constructor and 
+	then passing in the rows and then running the callback that is defined the loadAll method on the articlesController module. If not we are doing a getJSON call  to find the data in the hackeripsum file and then creating
+	a new callback function and passing in a parameter of rawData. Then we are Caching the json so we don't need to bring back in again. We are then instantiating each item on the new article from the json file. Then we are
+	calling the insert record method on the Article constructor. 
+	Finally we are accessing the insertRecord method on the prototype of the Article constructor. We are creating a function with a parameter of callback and then running webDbs execute method, which uses SQL. 
+	the sql command says to insert into the articles table each element in the constructor and setting the valuse of each as the respected keys. Then calling the callback which is next in the articlesController method. 
+	*/
     if (Article.all.length) {
       ctx.articles = Article.all;
       next();
